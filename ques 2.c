4b8686c7053d16a2d2f6fcc8b6ce4e847fb2684c
manya0702 <72210577+manya0702@users.noreply.github.com>
2020-11-30 16:49:38 +0530

    Add files via upload
    Write a C program to print all even numbers between 1 to 100.

---
+++ ques 2.c
@@ -0,0 +1,12 @@
+/* printing all even numbers form 1 to 100 */
+
+#include<stdio.h>
+int main()
+
+{ int i;
+printf("The even numbers between 1 to 100 are :");
+for(i=2;i<=100;i=i+2)
+    {printf(" \n %d",i);}
+
+return 0;
+}
